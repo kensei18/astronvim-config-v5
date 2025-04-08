4fa3a02586eea9ce78c6be7fa27b0dc38bebba1f
kensei.sakai <kensei.sakai@npc2407502.local>
2025-04-08 16:17:00 +0900

    fix resession config

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -5,7 +5,7 @@ return {
     local resession = require "resession"
     resession.add_hook("post_load", function() vim.cmd "Neotree dir=./" end)
     resession.add_hook("pre_load", function()
-      if vim.bo.filetype ~= "alpha" then resession.save(vim.fn.getcwd(), { dir = "dirsession" }) end
+      if vim.bo.filetype ~= "snacks_dashboard" then resession.save(vim.fn.getcwd(), { dir = "dirsession" }) end
     end)
   end,
 }
