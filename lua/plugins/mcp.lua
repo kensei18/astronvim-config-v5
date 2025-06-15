ccbfbcea2ccb55fb6e7f5c2668618f007bd166d4
kensei.sakai <kensei.sakai@Mac.localdomain>
2025-06-16 08:06:02 +0900

    remove avante.nvim temporarily

--- lua/plugins/mcp.lua
+++ lua/plugins/mcp.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "ravitemer/mcphub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    cmd = "MCPHub", -- lazy load by default
-    build = "npm install -g mcp-hub@latest",
-    config = function()
-      require("mcphub").setup {
-        extensions = {
-          avante = {
-            make_slash_commands = true,
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "ravitemer/mcphub.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   cmd = "MCPHub", -- lazy load by default
+  --   build = "npm install -g mcp-hub@latest",
+  --   config = function()
+  --     require("mcphub").setup {
+  --       extensions = {
+  --         avante = {
+  --           make_slash_commands = true,
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
