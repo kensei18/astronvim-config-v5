6bd701c1314dbed6b778695167f1a5a75d0520ec
kensei18 <kensei.sakai@gmail.com>
2025-06-16 19:08:29 +0900

    add render-markdown.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -6,4 +6,16 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     config = function() vim.fn["mkdp#util#install"]() end,
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.nvim",
+    },
+    opts = {
+      completions = {
+        lsp = { enabled = true },
+      },
+    },
+  },
 }
