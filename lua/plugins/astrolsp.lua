d55e70ca28ba42216bff2ce1d2524860f5a9174a
kensei.sakai <kensei.sakai@Mac.localdomain>
2025-08-08 11:32:27 +0900

    adjust lsp timeout

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -28,11 +28,23 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 3000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
     },
+    -- customize lsp file operations
+    -- file_operations = {
+    --   timeout = 10000, -- default timeout in ms for completing LSP operations
+    --   operations = { -- enable all of the file operations
+    --     willCreate = true,
+    --     didCreate = true,
+    --     willRename = true,
+    --     didRename = true,
+    --     willDelete = true,
+    --     didDelete = true,
+    --   },
+    -- },
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
