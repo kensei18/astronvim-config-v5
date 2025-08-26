4afff0651779e486f82a7dc16e3238fe6e5daa34
kensei.sakai <kensei.sakai@Mac.localdomain>
2025-08-26 10:22:51 +0900

    add quicker.nvim

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -1,7 +1,16 @@
 ---@type LazySpec
 return {
-  "kevinhwang91/nvim-bqf",
-  event = "VeryLazy",
-  ---@type BqfConfig
-  opts = {},
+  {
+    "kevinhwang91/nvim-bqf",
+    event = "VeryLazy",
+    ---@type BqfConfig
+    opts = {},
+  },
+  {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+  },
 }
